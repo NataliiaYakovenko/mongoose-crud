65165eac80dcbc90bab9075368f797c77eeb1771
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-09-10 10:32:52 +0300

    Add connection to module/index.js

--- model/index.js
+++ model/index.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
+const env = process.env.NODE_ENV ?? 'development';
+const {host, port, dbName} = require('./../config/mongoConfig.json')[env];
+
 
 mongoose
-  .connect('mongodb://localhost:27017/users_db')
+  .connect(`mongodb://${host}:${port}/${dbName}`)
   .then(() => console.log('DB Connection OK'))
   .catch((error) => console.log(error));
