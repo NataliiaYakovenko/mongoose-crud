865592abd838bc3f38b05ab8e9cc327d7f0b1651
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-09-10 11:26:31 +0300

    Add validation to model/User

---
+++ model/User.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const yup = require('yup');
+
+const EMAIL_VALIDATION_SCHEMA = yup.object({
+  email: yup.string().email(),
+});
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      match: /^[A-Z][a-z]+$/,
+      minLength: [2, 'First name must have more 2 letters'],
+      maxLength: 64,
+    },
+    lastNmame: {
+      type: String,
+      required: true,
+      match: /^[A-Z][a-z]+$/,
+      minLength: 2,
+      maxLength: 64,
+    },
+    email: {
+      type: String,
+      required: true,
+      validate: {
+        validator: (v) => EMAIL_VALIDATION_SCHEMA.isValid(v),
+      },
+    },
+    birthday: { type: Date, max: new Date() },
+    workExperience: { type: Number, min: 0 },
+    gender: { type: String, enum: ['male', 'female', 'other'] },
+    isMarried: { type: Boolean, default: false },
+  },
+  {
+    versionKey: false,
+  }
+);
+
+const User = mongoose.Model('User', userSchema);
+
+module.exports = User;
