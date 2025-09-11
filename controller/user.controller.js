99cf716f9ec8d1f7218ca3f32cebd7146fb22161
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-09-11 12:10:54 +0300

    Add getUserById, updateUser

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -20,18 +20,42 @@ module.exports.getUsers = async (req, res, next) => {
       .sort({ _id: 1 })
       .limit(limit)
       .skip(skip);
-    if (!foundUser) {
-      return next(getHttpError(404, 'Users not fiund'));
-    }
+
     return res.status(200).send({ data: foundUser });
   } catch (error) {
     next(error);
   }
 };
 
-module.exports.getUserById = async (req, res, next) => {};
+module.exports.getUserById = async (req, res, next) => {
+  const { userId } = req.params;
+  try {
+    const foundUserById = await User.findById(userId);
+    if (!foundUserById) {
+      return next(createHttpError(404, 'User not found'));
+    }
+    return res.status(200).send({ data: foundUserById });
+  } catch (error) {
+    next(error);
+  }
+};
 
-module.exports.updateUserById = async (req, res, next) => {};
+module.exports.updateUserById = async (req, res, next) => {
+  const { body } = req;
+  const { userId } = req.params;
+
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).send('User not found');
+    }
+
+    await user.updateOne(body);
+    return res.status(201).send({ data: user });
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports.deleteUserById = async (req, res, next) => {};
 
